4ce84a9ae5293fd7e01eac8fd2933d7bdfe33be6
Kayla Roth <kaylaroth353@gmail.com>
2023-12-16 19:15:49 -0500

    Exercise 5 Exercise 5

---
+++ Exercises 1,2,3/index.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#go').click(function(){
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        $('#jumbotron').text(fname + ' ' + lname);
+        return false;
+    })
+}); 
